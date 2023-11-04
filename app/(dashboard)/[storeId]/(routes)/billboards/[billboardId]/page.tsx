96896f0caf6d618dbaeec78c11a0edd3e5e9274b
MDawoodDeveloper <muhammaddawoodhr@gmail.com>
2023-11-04 09:58:51 +0500

    4 hour (format)

--- app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
@@ -2,22 +2,22 @@ import prismadb from "@/lib/prismadb";
 import { BillboardForm } from "./components/billboard-form";
 
 const billboardPage = async ({
-    params
+  params,
 }: {
-    params: { billboardId: string }
+  params: { billboardId: string };
 }) => {
-    const billboard = await prismadb.billboard.findUnique({
-        where: {
-            id: params.billboardId
-        }
-    });
-    return (
-        <div className="flex-col">
-            <div className="flex-1 space-y-4 p-8 pt-6">
-               <BillboardForm  initialData={billboard} />
-            </div>
-        </div>
-    );
-}
+  const billboard = await prismadb.billboard.findUnique({
+    where: {
+      id: params.billboardId,
+    },
+  });
+  return (
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <BillboardForm initialData={billboard} />
+      </div>
+    </div>
+  );
+};
 
 export default billboardPage;
